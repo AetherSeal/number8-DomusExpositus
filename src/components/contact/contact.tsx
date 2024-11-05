3ff33eba65f4d34d17c3eaffcf800dbcf062598b
Alejandro Castro <acastro@Alejandros-MacBook-Pro.local>
2024-11-04 22:53:10 -0300

    temporal commit

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,28 +1,56 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 export default function Contact() {
-  const { register } = useForm();
+  const contactFormSchema = z.object({
+    fullname: z
+      .string()
+      .min(6, { message: "Must be 6 or more characters long" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    phone: z.string().optional(),
+    comments: z
+      .string()
+      .min(10, { message: "Comments must be longer" })
+      .optional(),
+  });
+  const {
+    register,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(contactFormSchema) });
+
   return (
     <article className="bg-slate-500">
-      <h2>Contact Agent</h2>
+      <h2 className="flex justify-center items-center pt-8 text-white text-2xl">
+        Contact Agent
+      </h2>
       <form className="flex flex-col p-8 gap-4">
         <input
           {...register("fullname", {
-            required: "email is required",
+            required: "full name is required",
             minLength: 6,
           })}
           type="text"
           placeholder="Full Name"
           className="px-4 py-2 rounded"
         />
+        {errors.fullname && <p>{`${errors.fullname.message}`}</p>}
         <input
-          {...register("email")}
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+              message: "Invalid email",
+            },
+          })}
           type="email"
           name=""
           id=""
           placeholder="Email"
           className="px-4 py-2 rounded"
         />
+        {errors.email && <p>{`${errors.email.message}`}</p>}
+
         <input
           {...register("phone")}
           type="tel"
@@ -31,6 +59,8 @@ export default function Contact() {
           placeholder="Phone"
           className="px-4 py-2 rounded"
         />
+        {errors.phone && <p>{`${errors.phone.message}`}</p>}
+
         <textarea
           {...register("comments")}
           name=""
@@ -40,7 +70,10 @@ export default function Contact() {
           placeholder="Comments"
           className="px-4 py-2 rounded"
         ></textarea>
-        <button>Contact Now</button>
+        {errors.comments && <p>{`${errors.comments.message}`}</p>}
+        <button className="px-4 py-2 rounded bg-cyan-600 text-white shadow hover:scale-105 active:scale-95 transition-all">
+          Contact Now
+        </button>
       </form>
     </article>
   );
