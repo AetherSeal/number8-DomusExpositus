9ccf55ddd59c4b0ff5dc6bcb6efe9c8a4e6328a7
Alejandro Castro <alejandro.castro.q@gmail.com>
2024-11-06 15:33:36 -0300

    small keyframe animation added

--- src/components/Base/footer.tsx
+++ src/components/Base/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="bg-slate-700 p-4 flex text-white items-center justify-center ">
-      <p>Created by Alejandro Castro 💪</p>
+      <p>
+        Created by <span className="animate-glow">Alejandro Castro 💪</span>
+      </p>
     </footer>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        glow: "glow 5s infinite",
+      },
+      keyframes: {
+        glow: {
+          "0%": {
+            "text-shadow":
+              "0 0 5px #6666ff, 0 0 10px #6666ff, 0 0 15px #6666ff",
+          },
+          "10%": {
+            "text-shadow": "0 0 8px #0099ff",
+          },
+          "50%": {
+            "text-shadow":
+              "0 0 5px #00ff00, 0 0 10px #00ff00, 0 0 15px #00ff00",
+          },
+          "75%": {
+            "text-shadow": "0 0 10px #ff3399",
+          },
+          "100%": {
+            "text-shadow":
+              "0 0 5px #6666ff, 0 0 10px #6666ff, 0 0 15px #6666ff",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
